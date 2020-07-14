f9cc895a36a91a4703ce7b02e1489fc68252de4f
Corey Sanders <corey.sanders@otech.edu>
2020-07-14 08:17:59 +0000

    Finished Chapter 3

--- Projects/ReactBook/helloworld/public/App.js
+++ Projects/ReactBook/helloworld/public/App.js
@@ -57,12 +57,8 @@ var IssueRow = /*#__PURE__*/function (_React$Component2) {
   _createClass(IssueRow, [{
     key: "render",
     value: function render() {
-      var style = this.props.rowStyle;
-      return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
-        style: style
-      }, this.props.issue_id), /*#__PURE__*/React.createElement("td", {
-        style: style
-      }, this.props.children));
+      var issue = this.props.issue;
+      return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, issue.id), /*#__PURE__*/React.createElement("td", null, issue.status), /*#__PURE__*/React.createElement("td", null, issue.owner), /*#__PURE__*/React.createElement("td", null, issue.created.toDateString()), /*#__PURE__*/React.createElement("td", null, issue.effort), /*#__PURE__*/React.createElement("td", null, issue.due ? issue.due.toDateString() : ''), /*#__PURE__*/React.createElement("td", null, issue.title));
     }
   }]);
 
@@ -83,25 +79,34 @@ var IssueTable = /*#__PURE__*/function (_React$Component3) {
   _createClass(IssueTable, [{
     key: "render",
     value: function render() {
-      var rowStyle = {
-        border: "1px solid silver",
-        padding: 4
-      };
+      var issues = [{
+        id: 1,
+        status: 'New',
+        owner: 'Ravan',
+        efford: 5,
+        created: new Date('2018-08-15'),
+        due: undefined,
+        title: 'Error in console when '
+      }, {
+        id: 2,
+        status: 'Assigned',
+        owner: 'Eddie',
+        effort: 14,
+        created: new Date('2018-08-30'),
+        due: new Date('2018-08-30'),
+        title: 'Missing bottom border on panel'
+      }];
+      var issueRows = issues.map(function (issue) {
+        return /*#__PURE__*/React.createElement(IssueRow, {
+          key: issue.id,
+          issue: issue
+        });
+      });
       return /*#__PURE__*/React.createElement("table", {
         style: {
           borderCollapse: "collapse"
         }
-      }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", {
-        style: rowStyle
-      }, "ID"), /*#__PURE__*/React.createElement("td", {
-        style: rowStyle
-      }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement(IssueRow, {
-        issue_id: "1",
-        rowStyle: rowStyle
-      }, "Error in console when clicking Add"), /*#__PURE__*/React.createElement(IssueRow, {
-        issue_id: "2",
-        rowStyle: rowStyle
-      }, "Missing ", /*#__PURE__*/React.createElement("b", null, "bottom"), " border on panel")));
+      }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "ID"), /*#__PURE__*/React.createElement("th", null, "Status"), /*#__PURE__*/React.createElement("th", null, "Owner"), /*#__PURE__*/React.createElement("th", null, "Created"), /*#__PURE__*/React.createElement("th", null, "Effort"), /*#__PURE__*/React.createElement("th", null, "Due Date"), /*#__PURE__*/React.createElement("th", null, "Title"))), /*#__PURE__*/React.createElement("tbody", null, issueRows));
     }
   }]);
 
